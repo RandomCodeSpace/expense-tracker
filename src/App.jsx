3a1ab32b49952644fe3f00abba74206f46734d50
Amit Kumar <ak.nitrr13@gmail.com>
2023-08-20 21:56:46 +0530

    Added gitlab login

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from 'react';
+import {DatePicker, message} from 'antd';
+import './App.css';
+import GitLabOAuth from "./common/AppLogin.jsx";
 
-function App() {
-    const [count, setCount] = useState(0)
-
+export const App = () => {
+    const [date, setDate] = useState(null);
+    const [messageApi, contextHolder] = message.useMessage();
+    const handleChange = (value) => {
+        messageApi.info(`Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`);
+        setDate(value);
+    };
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo"/>
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo"/>
-                </a>
+        <div style={{width: 400, margin: '100px auto'}}>
+            <DatePicker onChange={handleChange}/>
+            <div style={{marginTop: 16}}>
+                Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
-    )
-}
-
-export default App
+            {contextHolder}
+            <GitLabOAuth/>
+        </div>
+    );
+};
---
+++ src/common/useLiveSessionState.jsx
@@ -0,0 +1,87 @@
+import {useEffect, useState} from "react"
+
+export const useBaseStorageState = (
+    storage,
+    storageKey,
+    defaultValue,
+    live = false
+) => {
+    // Get value from storage, if not use default value
+    const [state, setState] = useState(() => {
+        const storageValue = storage.getItem(storageKey)
+        return storageValue ? JSON.parse(storageValue) : defaultValue
+    })
+
+    useLiveStorageListners(
+        live,
+        handleStorageEvent(storageKey, setState),
+        storageKey
+    )
+
+    // On change set storage.
+    useEffect(() => {
+        storage.setItem(storageKey, JSON.stringify(state))
+    }, [storageKey, state])
+
+    const clear = () => {
+        storage.removeItem(storageKey)
+    }
+
+    const setStateExternal = setStateBuilder(state, setState, live, storageKey)
+
+    return [state, setStateExternal, clear]
+}
+
+export const useStorageState = (storageKey, defaultValue) =>
+    useBaseStorageState(localStorage, storageKey, defaultValue, false)
+
+export const useLiveStorageState = (storageKey, defaultValue) =>
+    useBaseStorageState(localStorage, storageKey, defaultValue, true)
+
+export const useSessionState = (storageKey, defaultValue) =>
+    useBaseStorageState(sessionStorage, storageKey, defaultValue, false)
+
+export const useLiveSessionState = (storageKey, defaultValue) =>
+    useBaseStorageState(sessionStorage, storageKey, defaultValue, true)
+
+function setStateBuilder(state, setState, live, storageKey) {
+    return newState => {
+        const oldValue = JSON.stringify(state)
+        setState(newState)
+        if (live) {
+            const event = new StorageEvent(`state-persist-${storageKey}`, {
+                key: storageKey,
+                oldValue,
+                newValue: JSON.stringify(newState)
+            })
+            window.dispatchEvent(event)
+        }
+    }
+}
+
+function useLiveStorageListners(live, handleStorageEvent, storageKey) {
+    useEffect(() => {
+        if (live) {
+            window.addEventListener("storage", handleStorageEvent)
+            window.addEventListener(`state-persist-${storageKey}`, handleStorageEvent)
+            return () => {
+                window.removeEventListener("storage", handleStorageEvent)
+                window.removeEventListener(
+                    `state-persist-${storageKey}`,
+                    handleStorageEvent
+                )
+            }
+        } else {
+            window.removeEventListener("storage", handleStorageEvent)
+        }
+        return
+    }, [live, storageKey])
+}
+
+export function handleStorageEvent(storageKey, setState) {
+    return evt => {
+        if (evt.key === storageKey && evt.newValue !== evt.oldValue) {
+            setState(evt.newValue ? JSON.parse(evt.newValue) : evt.newValue)
+        }
+    }
+}
